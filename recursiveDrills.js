559e1a602bcc75fcd3db2aef95a9391c792a6ac8
Derek Houck <derek@derekhouck.com>
2019-02-25 11:11:59 -0800

    More drills

--- recursiveDrills.js
+++ recursiveDrills.js
@@ -1,11 +1,13 @@
+'use strict';
+
 //Counting Sheep
 
 //Input: Number of Sheep
 //Output: Console Log num Another Sheep Jump The Fence
-function countingSheep(num){
+function countingSheep(num) {
   //base case
-  if(num === 0){
-      return console.log('All the sheep have jumped the fence');
+  if (num === 0) {
+    return console.log('All the sheep have jumped the fence');
   } else {
     //recursive case
     console.log(`${num} -Another Sheep jump the fence`);
@@ -20,9 +22,9 @@ function countingSheep(num){
 //Input: Array (unknown set of numbers)
 //Output: Array of numbers doubled
 //Will assume it's an array of numbers
-function arrayDouble(array){
+function arrayDouble(array) {
   //base case
-  if(array.length === 0){
+  if (array.length === 0) {
     return [];
   } else {
     return [array[0] * 2, ...arrayDouble(array.slice(1))];
@@ -35,13 +37,13 @@ function arrayDouble(array){
 //Input: String
 //Output: String reversed
 
-function reverseString(string){
+function reverseString(string) {
   //base case
-  if(string === ''){
+  if (string === '') {
     return '';
   } else {
-  //recursive case
-    return reverseString(string.slice(1)) + string[0]; 
+    //recursive case
+    return reverseString(string.slice(1)) + string[0];
   }
 }
 
@@ -50,11 +52,11 @@ function reverseString(string){
 //Input: 
 //Output:
 //Assumption: num is greater than 0
-function nthTriangularNumber(num){
-  if(num <= 0)
+function nthTriangularNumber(num) {
+  if (num <= 0)
     return Error('Must be greater than 0');
-  else if (num === 1){
-      return [1];
+  else if (num === 1) {
+    return [1];
   }
   else {
     return [...nthTriangularNumber(num - 1), (num * (num + 1) / 2)];
@@ -63,4 +65,46 @@ function nthTriangularNumber(num){
 
 // console.log(nthTriangularNumber(10));
 
+// String Splitter
+const stringSplitter = (string, separator) => {
+  const index = string.indexOf(separator);
+  if (index < 0) {
+    return [string];
+  } else {
+    return [string.slice(0, index), ...stringSplitter(string.slice(index + 1), separator)];
+  }
+};
 
+// console.log(stringSplitter('This is a sample string', ' '));
+// console.log(stringSplitter('This is a sample string', 'i'));
+
+// Binary Representation
+// Will be done in workshop
+
+// Factorial
+const factorial = num => {
+  if (num < 0) {
+    return 'Number should greater than or equal to zero';
+  } else if (num === 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+};
+
+// console.log(factorial(5));
+
+// Fibonacci
+const fibonacci = num => {
+  if (num < 0) {
+    return 'Number should greater than or equal to zero';
+  } else if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
+};
+
+console.log(fibonacci(14));
