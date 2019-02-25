399c70bfb4f822c9f4d009a6ac300e3ef87e58eb
nickJ <nicknotjustin@gmail.com>
2019-02-25 11:06:15 -0600

    Initial Commit. arrayDouble, reverseString, and nthTriangularNumber completed.

---
+++ recursiveDrills.js
@@ -0,0 +1,66 @@
+//Counting Sheep
+
+//Input: Number of Sheep
+//Output: Console Log num Another Sheep Jump The Fence
+function countingSheep(num){
+  //base case
+  if(num === 0){
+      return console.log('All the sheep have jumped the fence');
+  } else {
+    //recursive case
+    console.log(`${num} -Another Sheep jump the fence`);
+    return countingSheep(num - 1);
+  }
+}
+
+// countingSheep(3);
+
+//Array Double
+
+//Input: Array (unknown set of numbers)
+//Output: Array of numbers doubled
+//Will assume it's an array of numbers
+function arrayDouble(array){
+  //base case
+  if(array.length === 0){
+    return [];
+  } else {
+    return [array[0] * 2, ...arrayDouble(array.slice(1))];
+  }
+}
+
+// console.log(arrayDouble([1, 2, 3]));
+// console.log(arrayDouble([4, 5, 6]));
+
+//Input: String
+//Output: String reversed
+
+function reverseString(string){
+  //base case
+  if(string === ''){
+    return '';
+  } else {
+  //recursive case
+    return reverseString(string.slice(1)) + string[0]; 
+  }
+}
+
+// console.log(reverseString('RECURSIVESAREFUNOHMYGOD'));
+//1, 3, 6, 10, 15, 21, 28
+//Input: 
+//Output:
+//Assumption: num is greater than 0
+function nthTriangularNumber(num){
+  if(num <= 0)
+    return Error('Must be greater than 0');
+  else if (num === 1){
+      return [1];
+  }
+  else {
+    return [...nthTriangularNumber(num - 1), (num * (num + 1) / 2)];
+  }
+}
+
+// console.log(nthTriangularNumber(10));
+
+
